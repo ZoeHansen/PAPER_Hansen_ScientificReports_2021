13c0e73e1a798f1e2398666f136dc5a87eacd0ac
ZoeHansen <47985088+ZoeHansen@users.noreply.github.com>
2021-04-19 14:02:17 -0400

    Update Ecological_Statistics_CaseVsControl_Rcode.R

--- R_Analysis/Ecological_Statistics_CaseVsControl_Rcode.R
+++ R_Analysis/Ecological_Statistics_CaseVsControl_Rcode.R
@@ -11,12 +11,11 @@ library(ggplot2)
 library(ggpubr)
 library(vegan)
 library(calibrate)
-library(indicspecies)
 
-pheno <- read.csv('D://Manning_ERIN/CampylobacterSubset_AIM_ONE/Second_Analysis/Data_files_Hansen_2020/campylobacter_metadata_Hansen_2020.csv',
-                 header = TRUE, na.strings = c("", 'NA'))
+pheno <- read.csv('D://Manning_ERIN/CampylobacterSubset_AIM_ONE/campylobacter_metadata_casecontrol.csv',
+                 header = TRUE)
 
-data = read.csv('D://Manning_ERIN/CampylobacterSubset_AIM_ONE/Second_Analysis/Resistome_Data/Campy_fullgene_AGS_normal.csv', 
+data = read.csv('D://Manning_ERIN/CampylobacterSubset_AIM_ONE/Third_Analysis_ScientificReports_Submission/Resistome/campylobacter_casecontrol_RPKG_resistome_gene.csv', 
                 header = TRUE)
 
 ########################################################
@@ -25,42 +24,51 @@ data = read.csv('D://Manning_ERIN/CampylobacterSubset_AIM_ONE/Second_Analysis/Re
 
 #Extract the Cases and Controls from our dataset and remove IDs of excluded samples
 health <- pheno %>%
-  dplyr::select(ID, Case.status) %>%   
+  dplyr::select(ER_ID, Case.status, New.Family.ID, FamID.wStatus) %>% 
   drop_na()
+
+enviro <- pheno %>%
+  dplyr::select(ER_ID, Days.since.exposed.to.case, Age.years,Gender, Residence.type)
+
+meta <- left_join(health, enviro, by = 'ER_ID')
+meta[is.na(meta)] <- 0
 
 # Merge metadata with AGS-normalized abundances
 #Full Case_Control Campylobacter dataset with metadata of interest
-data_cc <- left_join(health, data, by = "ID")
+data_cc <- left_join(meta, data, by = "ER_ID")
 
 # Remove any lingering zeroes to avoid downstream errors
 data_cc <- data_cc[, colSums(data_cc != 0) > 0] 
 data_cc <- data_cc[rowSums(data_cc != 0) > 0,] 
 data_cc$Case.status <- factor(data_cc$Case.status)
+data_cc$New.Family.ID <-factor(data_cc$New.Family.ID)
+data_cc$FamID.wStatus <- factor(data_cc$FamID.wStatus)
 
 ########################################################
 # Calculate and plot within-sample (Alpha) Diversity
 ########################################################
 
 # Richness (of genes) across our samples
-r.c = specnumber(data_cc[,-c(1:2)])
+r.c = specnumber(data_cc[,-c(1:4)])
 
 # Shannon diversity
-h.c = diversity(data_cc[,-c(1:2)], index = 'shannon')
+h.c = diversity(data_cc[,-c(1:4)], index = 'shannon')
 
 # Calculate Pielou's Evenness index
 pielou.c = h.c/log(r.c)
 
 # Combine alpha diversity data and Case status information
-div.c=tibble(health$ID, health$Case.status, r.c, h.c, pielou.c)
-colnames(div.c)=c("ID", "Case.status", "Richness", "Shannon", "Pielou")
+div.c=tibble(health$ER_ID, health$Case.status, health$New.Family.ID, r.c, h.c, pielou.c)
+colnames(div.c)=c("ER_ID", "Case.status", "FamilyID",'Richness', "Shannon", "Pielou")
 
 ### Plot alpha diversity ###
 # (Figure 1)
 
 # Reshape the data to "long" format
-div.c.long=melt(div.c, id.vars=c("ID", "Case.status"))
+div.c.long=melt(div.c, id.vars=c("ER_ID", "Case.status", 'FamilyID'))
 
 # Determine the Mean, Min, and Max for these measures
+# Case status
 div.means <- div.c %>%
   group_by(Case.status) %>%
   summarise(MeanRich = mean(Richness), MeanShannon = mean(Shannon), MeanPielou = mean(Pielou)) 
@@ -69,6 +77,25 @@ div.minmax <- div.c %>%
   group_by(Case.status) %>%
   summarise(MinRich = min(Richness), MaxRich = max(Richness), MinS = min(Shannon), 
             MaxS = max(Shannon), MinP = min(Pielou), MaxP = max(Pielou))
+
+rich.comp.cc <- compare_means(Richness ~ Case.status, div.c, method = 'wilcox.test')
+shan.comp.cc <- compare_means(Shannon ~ Case.status, div.c, method = 'wilcox.test')
+even.comp.cc <- compare_means(Evenness ~ Case.status, div.c, method = 'wilcox.test')
+
+# Family ID
+fam.div.means <- div.c %>%
+  group_by(FamilyID) %>%
+  summarise(MeanRich = mean(Richness), MeanShannon = mean(Shannon), MeanPielou = mean(Pielou)) #%>%
+
+fam.div.minmax <- div.c %>%
+  group_by(FamilyID)%>%
+  summarise(MinRich=min(Richness), MaxRich=max(Richness), MinShannon=min(Shannon), MaxShannon=max(Shannon),
+            MinPielou=min(Pielou), MaxPielou=max(Pielou))
+
+# Statistical Comparisons
+rich.comp.fam <- compare_means(Richness ~ Family.ID, div.c, method = 'wilcox.test')
+shan.comp.fam <- compare_means(Shannon ~ Family.ID, div.c, method = 'wilcox.test')
+even.comp.fam <- compare_means(Evenness ~ Family.ID, div.c, method = 'wilcox.test')
 
 my_comp_cc  <- list(c('Case','Control'))
 colors_cc = c('cyan4', 'darkorange3')
@@ -101,7 +128,7 @@ ggplot(data=div.c.long, aes(x=Case.status, y=value))+
 ########################################################
 
 #Calculate Bray-curtis dissimilarity. 
-bc.d.c=vegdist(data_cc[,-c(1:4)], method="bray")
+bc.d.c=vegdist(data_cc[,-c(1:8)], method="bray")
 
 
 #Map desired colors to the Case statuses to make our future legend for the PCoA.  
@@ -109,10 +136,11 @@ Class=rep(1 ,nrow(data_cc))
 Class[data_cc$Case.status=="Case"]= as.integer(21) #'cyan4'
 Class[data_cc$Case.status=="Control"]= as.integer(24) #'darkorange3'
 
-# Identify the controls with Abx treatment:
-Abx=rep(1, nrow(data_cc))
-Abx[data_cc$Abx.use == 'Yes']=24
-Abx[data_cc$Abx.use == 'No']=21
+# Identify cases vs. controls and those who received Abx treatment:
+status=rep(1,nrow(data_cc))
+status[data_cc$Case.status=='Case']=21
+status[data_cc$Case.status=='Control']=24
+status[data_cc$Abx.use == 'Yes']=22
 
 # For PCoA indicating Family ID (16 total families):
 fam = rep('black', nrow(data_cc))
@@ -141,65 +169,42 @@ bc.c.pcoa=cmdscale(bc.d.c, eig=TRUE) # for data_cc
 ax1.v.bc.c=bc.c.pcoa$eig[1]/sum(bc.c.pcoa$eig)
 ax2.v.bc.c=bc.c.pcoa$eig[2]/sum(bc.c.pcoa$eig)
 
+
+# We can add other environmental factors, but for now just leave them out
+env.sub= data_cc[, c('Gender', 'Days.since.exposed.to.case','Residence.type','Age.years')]
+envEF.bc=envfit(bc.c.pcoa, env.sub, permutations = 999, na.rm = TRUE)
+envEF.bc
+
+### Note: Change "rownames(envEF.bc$factors$centroids)" to alter the factor levels in plot
+
 # Plot the ordination.
 plot(bc.c.pcoa$points[,1],bc.c.pcoa$points[,2],
      cex=1.5,pch=Class,
-     bg=Res,
+     bg=fam,
      xlab= paste("PCoA1: ",100*round(ax1.v.bc.c,3),"% var. explained",sep=""), 
      ylab= paste("PCoA2: ",100*round(ax2.v.bc.c,3),"%var. explained",sep=""),
-     xlim=c(-0.7,0.4),
-     ylim=c(-0.5, 0.5))
+     xlim=c(-0.75,0.65),
+     ylim=c(-0.45, 0.45))
 
-legend('topright', c('Case','Control','Received Antibiotics'), pch = c(21, 21, 2), 
+plot(envEF.bc, p.max=0.05, col='black', labels=list(vectors=paste('Days Since Exposed to Case')))
+
+legend('topright', c('Case','Control','Received Antibiotics'), pch = c(21, 24, 22), 
        pt.bg=c('cyan4', 'darkorange3', 'black'), lty=0)
 
 #Add ID labels to points (if desired)
-textxy(X=bc.c.pcoa$points[,1], Y=bc.c.pcoa$points[,2],labs=data_cc$New.Family.ID, cex=0.6)
+textxy(X=bc.c.pcoa$points[,1], Y=bc.c.pcoa$points[,2],labs=data_cc$FamID.wStatus, cex=0.6)
 
 
 ########################################################
 # PERMANOVA
 ########################################################
 # Centroid
-a.c = adonis(bc.d.c~Class, distance=TRUE, permutations=1000)
+a.c = adonis(bc.d.c~data.cc$Case.status, distance=TRUE, permutations=1000) # Repeat for Family ID
 
 # Disperson
-b.c=betadisper(bc.d.c, group=Class)
+b.c=betadisper(bc.d.c, group=data_cc$Case.status) #Repeat for Family ID
+permutest(b.c)
 
 # Post-hoc Tukey's Honestly Significant Difference 
 TukeyHSD(b.c, which = "group", ordered = FALSE,conf.level = 0.95)
-
-
-# Performing a pairwise PERMANOVA:
-# (from P. Martinez Arbizu: https://github.com/pmartinezarbizu/pairwiseAdonis)
-pairwise.adonis <- function(x,factors, sim.method, p.adjust.m)
-{
-  library(vegan)
-  co = as.matrix(combn(unique(factors),2))
-  pairs = c()
-  F.Model =c()
-  R2 = c()
-  p.value = c()
-  for(elem in 1:ncol(co)){
-    ad = adonis(x[factors %in% c(as.character(co[1,elem]),as.character(co[2,elem])),] 
-                ~ factors[factors %in% c(as.character(co[1,elem]),(co[2,elem]))] , method =sim.method);
-    pairs = c(pairs,paste(co[1,elem],'vs',co[2,elem]));
-    F.Model =c(F.Model,ad$aov.tab[1,4]);
-    R2 = c(R2,ad$aov.tab[1,5]);
-    p.value = c(p.value,ad$aov.tab[1,6])
-  }
-  p.adjusted = p.adjust(p.value,method=p.adjust.m)
-  pairw.res = data.frame(pairs,F.Model,R2,p.value,p.adjusted)
-  return(pairw.res)
-}
-
-PW.Adonis= pairwise.adonis(x, factors, sim.method = 'bray', p.adjust.m='bonferroni')
-
-
-########################################################
-# Indicator Species Analysis
-########################################################
-
-indval = multipatt(data_cc[,-c(1:6)], cluster = data_cc$Case.status, control=how(nperm=999))
-summary(indval, indvalcomp = TRUE)
 
